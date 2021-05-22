48e969390080569ddf7606b1e3ec4a5dc64793c8
clarscent <clarscents@sjinc.co.kr>
2021-05-22 15:39:56 +0900

    .

--- src/java/egovframework/platform/module/kobis/controller/KobisController.java
+++ src/java/egovframework/platform/module/kobis/controller/KobisController.java
@@ -2,8 +2,8 @@ package egovframework.platform.module.kobis.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import egovframework.platform.module.base.BaseRestController;
+import egovframework.platform.module.kobis.vo.MovieListResultVO;
 import egovframework.platform.module.kobis.service.KobisService;
-import egovframework.platform.module.kobis.vo.KobisVO;
 import egovframework.platform.util.Constants;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
@@ -18,11 +18,7 @@ public class KobisController extends BaseRestController {
 	@Resource(name = "kobisService")
 	private KobisService kobisService;
 
-	/** Validator 검토필요 */
-	/*@Resource(name = "beanValidator")
-	protected DefaultBeanValidator beanValidator;*/
-
-	private final String BASE_URL = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieList.json";
+	private final String BASE_URL = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/";
 	private final String SERVICE_KEY = "809a07f4b41508becc428d67efe8446d";
 
 	public KobisController() {
@@ -30,35 +26,32 @@ public class KobisController extends BaseRestController {
 		logger = LoggerFactory.getLogger(this.getClass());
 	}
 
-	// 기상정보 GET
-	//@RequestMapping(value="/getMovieList/{x}/{y}", method = RequestMethod.GET)
 	@RequestMapping(value="/getMovieList", method = RequestMethod.GET)
 	public @ResponseBody
-	//KobisVO getKma(@PathVariable("x") String x, @PathVariable("y") String y) {
-	String getMovieList() {
+	MovieListResultVO getMovieList() {
 		logger.info("requestURL:" + context.getRequestURL());
-		KobisVO kobisVO = null;
+		MovieListResultVO movieList = null;
 
 		String result = "";
 
 		try {
-			String MOVIE_URL = BASE_URL + "?key=" + SERVICE_KEY;
+			String MOVIE_URL = BASE_URL + "movie/searchMovieList.json?key=" + SERVICE_KEY;
 			MOVIE_URL += "&curPage=1";
+			//MOVIE_URL += "&movieNm=" + movieNm;
 
 			URL url = new URL(MOVIE_URL);
 
 			RestTemplate rt = new RestTemplate();
 			ObjectMapper mapper = new ObjectMapper();
 
-			//kobisVO = mapper.readValue(rt.getForObject(url.toURI(), String.class), KobisVO.class);
+			//result = rt.getForObject(url.toURI(), String.class);
 
-			result = rt.getForObject(url.toURI(), String.class); // 데이터 수신후 무조건 호출
-
-			System.out.println("result\n" + result);
+			movieList = mapper.readValue(rt.getForObject(url.toURI(), String.class), MovieListResultVO.class);
+			System.out.println(movieList);
 		} catch (Exception e) {
 			logger.error(Constants.ERROR_CON, e);
 		}
 
-		return result;
+		return movieList;
 	}
 }
